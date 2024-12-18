29c718aad21143ad510f264e1f4d73b572f9d845
bihurin31204 <bihurinsalsabilafirdaus@gmail.com>
2024-12-18 14:05:50 +0700

    FIX

--- prisma/migrations/20241208182949_add_city_distances/migration.sql
+++
@@ -1,9 +0,0 @@
--- CreateTable
-CREATE TABLE "CityDistance" (
-    "id" SERIAL NOT NULL,
-    "origin" TEXT NOT NULL,
-    "destination" TEXT NOT NULL,
-    "distance" INTEGER NOT NULL,
-
-    CONSTRAINT "CityDistance_pkey" PRIMARY KEY ("id")
-);
